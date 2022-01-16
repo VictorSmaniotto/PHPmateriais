99b547bc09d7b5e11f0840d7a96d9bf5324dcf61
VictorSmaniotto <victorsmaniotto@hotmail.com>
2022-01-16 11:57:51 -0300

    Exercício 10 concluído

---
+++ exercicios/exercicio10.php
@@ -0,0 +1,22 @@
+<?php 
+
+    /*Crie uma operação que utiliza subtração (-), divisão (/) e multiplicação (*) 
+    Armazene todos os valores em variáveis
+    Imprima o resultado final*/
+
+    $a = -3;
+    $b = 5;
+    $c = 9;
+    $total = 0;
+
+    $total = ($b - $a) / $c * 0.8;
+    echo $total;
+    echo "<br>";
+
+    $total = $a * $c - $b / $a;
+    echo $total;
+    echo "<br>";
+
+    
+
+?>
