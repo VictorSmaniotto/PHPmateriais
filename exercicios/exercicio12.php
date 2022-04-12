3ebb2523eba03425fc3f03c5beab33ad13e33928
VictorSmaniotto <victorsmaniotto@hotmail.com>
2022-04-12 19:12:56 -0300

    Exercício finalizado sobre operadores

---
+++ exercicios/exercicio12.php
@@ -0,0 +1,31 @@
+<?php
+
+    /*Crie uma operação com cada um dos operadores básicos
+    Cara operação deve estar em uma variável diferente;*/
+
+    $soma = 25 + 68;
+    echo $soma . "<br>";
+
+    $subtracao = 149 - 282;
+    echo $subtracao . "<br>";
+
+    $multiplicacao = 165 * 225;
+    echo $multiplicacao . "<br>";
+
+    $divisao = 65 / 7;
+    echo $divisao . "<br>";
+
+    $num1 = 63;
+    $num2 = 39;
+
+    $opSoma = $num1 + $num2;
+    echo $opSoma . " Soma <br>";
+
+    $opSubtracao = $num1 - $num2;
+    echo $opSubtracao . " Subtração <br>";
+
+    $opMultiplicacao = $num1 * $num2;
+    echo $opMultiplicacao . " Multiplicação <br>";
+
+    $opDivisao = $num1 / $num2;
+    echo $opDivisao . " Divisão <br>";
